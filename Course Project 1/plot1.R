eaf81c632dd0740da7d67095ec8886af3cbb23bd
wilsonow <wilsonow1989@outlook.com>
2014-11-10 11:09:42 +0800

    plot1 R code

--- Course Project 1/plot1.R
+++ Course Project 1/plot1.R
@@ -1,3 +1,4 @@
+
 ## Getting full dataset
 data_full <- read.csv("./Data/household_power_consumption.txt", header=T, sep=';', na.strings="?", 
                       nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
